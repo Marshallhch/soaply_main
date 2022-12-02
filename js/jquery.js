1f0c263bfa99f83a7d13f6059076a8416ebe4911
Marshallhch <marshallhch@gmail.com>
2022-12-02 12:51:05 +0900

    instagram infomation section

---
+++ js/jquery.js
@@ -0,0 +1,43 @@
+// $(document).ready(function () {
+//   // do something...
+// }); // not recommended...
+
+$(function () {
+  //   // Masonry Effect
+  //   $(".grid").masonry({
+  //     // options
+  //     itemSelector: ".grid-item",
+  //     // columnWidth: 200,
+  //   });
+
+  //   const gridBox = $(".grid");
+  //   const gridBox1 = document.querySelector(".grid");
+  //   console.log(gridBox1);
+  //   console.log(gridBox);
+
+  const getGalleryData = (data) => {
+    // console.log(data);
+    let items = [];
+    $.each(data, function (i, item) {
+      // console.log(i);
+      // console.log(item);
+
+      const galleryItems = `
+            <div class="grid-item"><img src="/images/${item.datamain}" alt=""></div>
+        `;
+      items.push($(galleryItems).get(0));
+      // $(".grid").append($(galleryItems).get(0));
+    });
+    $(".grid").append(items);
+    $(".grid").imagesLoaded(function () {
+      // Masonry Effect
+      $(".grid").masonry({
+        // options
+        itemSelector: ".grid-item",
+        // columnWidth: 200,
+      });
+    });
+  };
+
+  $.getJSON("/data/gallery.json", getGalleryData);
+}); // recommended...
