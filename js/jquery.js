210f2330cbf077600ac9fb47ed8c9bebbf91c768
Marshallhch <marshallhch@gmail.com>
2022-12-06 12:51:43 +0900

    main project ui complete

--- js/jquery.js
+++ js/jquery.js
@@ -40,4 +40,12 @@ $(function () {
   };
 
   $.getJSON("/data/gallery.json", getGalleryData);
+
+  // Navigation Moving to Target Section
+  $(".nav-lists li").on("click", function () {
+    const targetIdx = $(this).index();
+    const pagePosition = $(".nav-target").eq(targetIdx).offset().top;
+
+    $("html, body").animate({ scrollTop: pagePosition }, 300);
+  });
 }); // recommended...
