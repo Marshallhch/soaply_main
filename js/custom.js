126d95f63e45ee21ae66d355291f4af24c83ee3d
Marshallhch <marshallhch@gmail.com>
2022-12-28 12:23:41 +0900

    detail page UI...

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,9 @@
-window.addEventListener("load", function () {
-  /********** Header Change Effect **********/
-  const header = document.querySelector("#header");
-  const stickyHeader = () => {
-    const scry = window.scrollY;
-    if (scry > 0) header.classList.add("active");
-    else header.classList.remove("active");
-  };
-
-  window.addEventListener("scroll", stickyHeader);
-});
-
 /********** Get Products Json Data **********/
 const productsBox = document.querySelector(".products");
 
 const getData = async () => {
   // /main_poject/data/products.json
-  await fetch("/main_backend/model/get_products.php")
+  await fetch("/main_backend/model/get_products.php?limit=all")
     .then((response) => {
       // console.log(response);
       return response.json();
@@ -28,12 +16,18 @@ const getData = async () => {
         dataEl = `
             <div class="product-frame">
               <div class="product-item">
-                  <img src="/main_poject/images/products/${item.pro_img}" alt="">
+                  <img src="/main_poject/images/products/${
+                    item.pro_img
+                  }" alt="">
                   <div class="product-text">
                       <h4>${item.pro_name}</h4>
-                      <strong>${item.pro_price}</strong>
+                      <strong>${item.pro_price
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</strong>
                       <p>${item.pro_desc}</p>
-                      <a href="/main_poject/pages/details.html?idx=${item.pro_idx}" class="common-btn">자세히 보기</a>
+                      <a href="/main_poject/pages/details.html?idx=${
+                        item.pro_idx
+                      }" class="common-btn">자세히 보기</a>
                   </div>
               </div>
             </div>
@@ -54,27 +48,32 @@ window.addEventListener("resize", () => {
   document.querySelector("#fh").style.height = instaImgHeight + "px";
 });
 
-/********** Scrollreveal Effect **********/
-const sr = ScrollReveal({
-  reset: false,
-});
+window.addEventListener("load", function () {
+  /********** Scrollreveal Effect **********/
+  const sr = ScrollReveal({
+    reset: false,
+  });
 
-sr.reveal(".wrapper", { duration: 1000 });
-sr.reveal(".landing-text-box", {
-  duration: 1000,
-  origin: "right",
-  distance: "80px",
-});
-sr.reveal(".meet-text-box, .swiper, .review-frame, .products", {
-  duration: 1000,
-  origin: "bottom",
-  distance: "40px",
-});
-sr.reveal(".meet-wrapper img, .feature", {
-  duration: 1000,
-  origin: "bottom",
-  distance: "40px",
-  interval: 200,
+  sr.reveal(".wrapper", { duration: 1000 });
+  sr.reveal(".landing-text-box", {
+    duration: 1000,
+    origin: "right",
+    distance: "80px",
+  });
+  sr.reveal(".meet-text-box, .swiper, .products", {
+    duration: 1000,
+    origin: "bottom",
+    distance: "40px",
+  });
+  sr.reveal(
+    ".meet-wrapper img, .feature, .product-frame, .review-frame, .grid-item",
+    {
+      duration: 1000,
+      origin: "bottom",
+      distance: "40px",
+      interval: 300,
+    }
+  );
 });
 
 /********** Swiper Slider Effect **********/
